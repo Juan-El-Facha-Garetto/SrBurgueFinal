22ce30830a4c3616e5f6b2976280d1ef263cca27
Juan Ignacio Garetto <contacto.garettojuanignacio@gmail.com>
2025-06-26 23:49:05 -0300

    a ver

---
+++ backend/conexion.js
@@ -0,0 +1,24 @@
+import mssql from 'mssql';
+
+const connectionSettings = {
+    server: 'localhost',
+    database: 'BurguersFinal1',
+    user: 'Juan',
+    password: 'Juan123',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    }
+};
+
+export async function getConnection() {
+    try {
+        return await mssql.connect(connectionSettings);
+        
+    }
+   catch (error) {
+        console.error("Error connecting to the database: ", error);
+        throw error;
+    }
+}
+export {mssql};
