e04af09fb3ac00467634283401ef422586b3ffaf
Juan Ignacio Garetto <contacto.garettojuanignacio@gmail.com>
2025-07-12 12:46:05 -0300

    listo

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { agregarUsuario, loginUsuario } from '../controllers/UsuarioController.js';
+import { loginUsuario } from '../controllers/UsuarioController.js';
 
 const router = express.Router();
 
-router.post('/register', agregarUsuario);
+
 router.post('/login', loginUsuario);
 
 export default router;
